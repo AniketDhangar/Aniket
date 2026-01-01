d3d586b1b78f93a21abcf8dd30ae8d32e53ffc46
Aniket Dhangar <147258918+AniketDhangar@users.noreply.github.com>
2026-01-01 12:18:05 +0530

    Update resume download link and filename

--- src/layout/FloatingResumeButton.jsx
+++ src/layout/FloatingResumeButton.jsx
@@ -3,8 +3,8 @@ import { Download } from "lucide-react";
 export default function FloatingResumeButton() {
   return (
     <a
-      href="/Aniket_Dhangar_MERN_Stack_Resume.pdf" // Path to your resume in public folder
-      download="Aniket_Dhangar_Resume.pdf" // Default downloaded file name
+      href="/Aniket_Dhangar_Backend_NodeJs_Developer.pdf" // Path to your resume in public folder
+      download="Aniket_Dhangar_Backend_NodeJs_Developer.pdf" // Default downloaded file name
       className="fixed bottom-6 right-6 z-50 flex items-center gap-2 bg-[#4A90E2] hover:bg-[#357ABD] text-white font-semibold px-4 py-3 rounded-full shadow-lg transition-all duration-300 cursor-pointer"
       title="Download Resume"
       
